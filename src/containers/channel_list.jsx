856b143bdbf1741607b9ca64b01d7a0074063462
Ryan Woods <contact@ryanofwoods.com>
2021-04-02 12:19:46 +0200

    create app skeleton with base css

---
+++ src/containers/channel_list.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class ChannelList extends Component {
+  render () {
+    return (
+      <div className="channel-list" />
+    );
+  }
+}
+
+export default ChannelList;
---
+++ src/containers/message_list.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div className="message-list" />
+    );
+  }
+}
+
+export default MessageList;
